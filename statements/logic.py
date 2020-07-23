e2129bfde4c518c517a77b64015e1e6067e34fb7
Alex <alexwork13@ukr.net>
2020-07-24 00:21:39 +0300

    first task

---
+++ statements/logic.py
@@ -0,0 +1,8 @@
+st = 'Print only the words that start with s in this sentence'
+
+list1 = st.split(' ')
+print(list1)
+for item in list1:
+    list2 = [x for x in item ]
+    if list2[0] == 's':
+        print(item)
